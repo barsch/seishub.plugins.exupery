1f545f5e0dbdd7f23760998a9f5d303e3d87c980
Robert Barsch <mail@robert-barsch.de>
2009-06-16 11:52:13 +0000

    - SQLView for seismic station activity - bugfix filemonitor

--- seishub/plugins/exupery/seismic.py
+++ seishub/plugins/exupery/seismic.py
@@ -15,6 +15,7 @@ from seishub.packages.interfaces import ISQLView, IMapper
 from seishub.util.xmlwrapper import toString
 from sqlalchemy import sql
 from seishub.registry.defaults import miniseed_tab
+from seishub.xmldb.defaults import document_tab
 
 
 class SeismicStationSQLView(Component):
@@ -22,26 +23,26 @@ class SeismicStationSQLView(Component):
     Seismic station distribution layer.
     """
     implements(ISQLView)
-    
+
     view_id = 'gis_seismic-station'
-    
+
     def createView(self):
         # filter indexes
         catalog = self.env.catalog.index_catalog
         xmlindex_list = catalog.getIndexes('seismology', 'station')
-        
-        filter = ['network_id', 'location_id', 'station_id', 'latitude', 
-                  'longitude', 'start_datetime', 'end_datetime'] 
+
+        filter = ['network_id', 'location_id', 'station_id', 'latitude',
+                  'longitude', 'start_datetime', 'end_datetime']
         xmlindex_list = [x for x in xmlindex_list if x.label in filter]
         if not xmlindex_list:
             return
         # build up query
-        query, joins = catalog._createIndexView(xmlindex_list, compact = True)
-        
+        query, joins = catalog._createIndexView(xmlindex_list, compact=True)
+
         options = [
             sql.func.random().label("random"),
             sql.func.GeomFromText(
-                sql.text("'POINT(' || longitude.keyval || ' ' || " +\
+                sql.text("'POINT(' || longitude.keyval || ' ' || " + \
                          "latitude.keyval || ')', 4326")).label('geom')
         ]
         for option in options:
@@ -55,36 +56,59 @@ class SeismicStationActivitySQLView(Component):
     Seismic station distribution layer.
     """
     implements(ISQLView)
-    
+
     view_id = 'gis_seismic-station-activity'
-    
+
     def createView(self):
         # filter indexes
         catalog = self.env.catalog.index_catalog
         xmlindex_list = catalog.getIndexes('seismology', 'station')
-        
+
         filter = ['network_id', 'location_id', 'station_id', 'channel_id',
-                  'latitude', 'longitude', 'start_datetime', 'end_datetime'] 
+                  'latitude', 'longitude', 'start_datetime', 'end_datetime']
         xmlindex_list = [x for x in xmlindex_list if x.label in filter]
         if not xmlindex_list:
             return
         # build up query
-        query, joins = catalog._createIndexView(xmlindex_list, compact = True)
-        
+        query, joins = catalog._createIndexView(xmlindex_list, compact=True)
+
         options = [
-            miniseed_tab.c.end_datetime.label("latency"),
+            sql.functions.max(miniseed_tab.c.end_datetime).label("latest_activity"),
+            (sql.func.now() - sql.functions.max(miniseed_tab.c.end_datetime)).label("latency"),
             sql.func.random().label("random"),
-#            sql.func.GeomFromText(
-#                sql.text("'POINT(' || longitude.keyval || ' ' || " +\
-#                         "latitude.keyval || ')', 4326")).label('geom')
+            sql.func.GeomFromText(
+                sql.text("'POINT(' || longitude.keyval || ' ' || " + \
+                         "latitude.keyval || ')', 4326")).label('geom')
         ]
         for option in options:
             query.append_column(option)
-        import pdb;pdb.set_trace()
-        oncl = (miniseed_tab.c['channel_id'] == sql.literal_column("channel_id.keyval"))
-        joins = document_tab.join(resource_tab, onclause = oncl)
-        
-        query = query.select_from(joins)
+        oncl = miniseed_tab.c['network_id'] == sql.literal_column("network_id.keyval")
+        oncl = sql.and_(oncl, miniseed_tab.c['station_id'] == sql.literal_column("station_id.keyval"))
+        oncl = sql.and_(oncl, miniseed_tab.c['channel_id'] == sql.literal_column("channel_id.keyval"))
+        oncl = sql.and_(oncl, sql.or_(
+            miniseed_tab.c['location_id'] == sql.literal_column("location_id.keyval"),
+            sql.and_(
+                miniseed_tab.c['location_id'] == None,
+                sql.literal_column("location_id.keyval") == None
+            )))
+        oncl = sql.and_(oncl, miniseed_tab.c['end_datetime'] > sql.literal_column("start_datetime.keyval"))
+        oncl = sql.and_(oncl, sql.or_(
+            miniseed_tab.c['end_datetime'] <= sql.literal_column("end_datetime.keyval"),
+            sql.literal_column("end_datetime.keyval") == None
+            ))
+
+
+        joins = joins.join(miniseed_tab, onclause=oncl)
+        query = query.select_from(joins).group_by(
+            document_tab.c.id,
+            sql.literal_column("station_id.keyval"),
+            sql.literal_column("channel_id.keyval"),
+            sql.literal_column("network_id.keyval"),
+            sql.literal_column("location_id.keyval"),
+            sql.literal_column("latitude.keyval"),
+            sql.literal_column("longitude.keyval"),
+            sql.literal_column("start_datetime.keyval"),
+            sql.literal_column("end_datetime.keyval"))
         return util.compileStatement(query)
 
 
@@ -93,31 +117,31 @@ class SeismicEventSQLView(Component):
     Creates SQL View for GPS Data components.
     """
     implements(ISQLView)
-    
+
     view_id = 'gis_seismic-event'
-    
+
     def createView(self):
         # filter indexes
         catalog = self.env.catalog.index_catalog
         xmlindex_list = catalog.getIndexes('seismology', 'event')
-        filter = ['datetime', 'latitude', 'longitude', 'depth', 
+        filter = ['datetime', 'latitude', 'longitude', 'depth',
                   'magnitude', 'magnitude_type', 'event_type', 'np1_strike',
-                  'np1_dip', 'np1_rake', 'mt_mrr', 'mt_mtt', 'mt_mpp', 
-                  'mt_mrt', 'mt_mrp', 'mt_mtp', 'localisation_method'] 
+                  'np1_dip', 'np1_rake', 'mt_mrr', 'mt_mtt', 'mt_mpp',
+                  'mt_mrt', 'mt_mrp', 'mt_mtp', 'localisation_method']
         xmlindex_list = [x for x in xmlindex_list if x.label in filter]
         if not xmlindex_list:
             return
         # build up query
-        query, joins = catalog._createIndexView(xmlindex_list, compact = True)
+        query, joins = catalog._createIndexView(xmlindex_list, compact=True)
         options = [
             sql.literal_column("datetime.keyval").label("end_datetime"),
             sql.literal_column("datetime.keyval").label("start_datetime"),
             sql.case(
                 value=sql.literal_column("localisation_method.keyval"),
-                whens = {'manual': 'circle'}, 
-                else_ = 'square').label('gis_localisation_method'), 
+                whens={'manual': 'circle'},
+                else_='square').label('gis_localisation_method'),
             sql.func.GeomFromText(
-                sql.text("'POINT(' || longitude.keyval || ' ' || " +\
+                sql.text("'POINT(' || longitude.keyval || ' ' || " + \
                          "latitude.keyval || ')', 4326")).label('geom')
         ]
         for option in options:
@@ -131,9 +155,9 @@ class BeachballMapper(Component):
     Returns a beachball image for the given parameters.
     """
     implements(IMapper)
-    
+
     mapping_url = '/exupery/wp3/seismic/event/beachball'
-    
+
     def process_GET(self, request):
         from obspy.imaging.beachball import Beachball
         # parse input arguments
@@ -143,10 +167,10 @@ class BeachballMapper(Component):
             alpha = float(request.args0.get('alpha', 0.8))
             linewidth = float(request.args0.get('linewidth', 2))
             # try to parse fm
-            if fm.count(',')==2:
+            if fm.count(',') == 2:
                 fm = fm.split(',')
                 fm = [float(fm[0]), float(fm[1]), float(fm[2])]
-            elif fm.count(',')==5:
+            elif fm.count(',') == 5:
                 fm = fm.split(',')
                 fm = [float(fm[0]), float(fm[1]), float(fm[2]),
                       float(fm[3]), float(fm[4]), float(fm[5])]
@@ -154,17 +178,17 @@ class BeachballMapper(Component):
                 return ''
         except:
             return ''
-        if alpha<0 or alpha>1:
+        if alpha < 0 or alpha > 1:
             alpha = 1
-        if linewidth<0 or linewidth>10:
+        if linewidth < 0 or linewidth > 10:
             linewidth = 2
-        if size<100 or size>1000:
+        if size < 100 or size > 1000:
             size = 100
-        
+
         # generate correct header
         request.setHeader('content-type', 'image/svg+xml; charset=UTF-8')
         # create beachball
-        return Beachball(fm, size, format='svg', alpha=alpha, 
+        return Beachball(fm, size, format='svg', alpha=alpha,
                          linewidth=linewidth)
 
 
@@ -173,10 +197,10 @@ class SeismicEventTypeMapper(Component):
     Returns a list of seismic event types over a certain time span.
     """
     implements(IMapper)
-    
+
     package_id = 'exupery'
     mapping_url = '/exupery/wp3/seismic/event/eventtype'
-    
+
     def process_GET(self, request):
         # parse input arguments
         args = {}
@@ -184,7 +208,7 @@ class SeismicEventTypeMapper(Component):
         args['vid'] = request.args0.get('volcano_id', '')
         args['start'] = request.args0.get('start_datetime', 'NOW()')
         args['end'] = request.args0.get('end_datetime', 'NOW()')
-        
+
         # generate XML result
         xml = Element("query")
         # build up and execute query
@@ -199,7 +223,7 @@ class SeismicEventTypeMapper(Component):
             result = self.env.db.query(query, **args)
         except:
             return toString(xml)
-        
+
         for res in result:
             if not res.event_type:
                 continue
