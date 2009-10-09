c566d773e2078d1d15bd105c1e784dd6c1475e1b
Robert Barsch <mail@robert-barsch.de>
2009-10-09 14:55:46 +0000

    - updated IBIS mapper

--- seishub/plugins/exupery/ibis.py
+++ seishub/plugins/exupery/ibis.py
@@ -16,14 +16,14 @@ GIS Layer:
 """
 
 from lxml.etree import Element, SubElement as Sub
+from obspy.core import UTCDateTime
 from seishub.core import Component, implements
 from seishub.packages.installer import registerIndex, registerSchema, \
     registerStylesheet
 from seishub.packages.interfaces import IResourceType, IMapper
 from seishub.util.xmlwrapper import toString
-from sqlalchemy import sql
+from sqlalchemy import Table, sql
 import os
-from obspy.core import UTCDateTime
 
 
 class IBISResourceType(Component):
@@ -31,37 +31,41 @@ class IBISResourceType(Component):
     IBIS resource type.
     """
     implements(IResourceType)
-    
+
     package_id = 'exupery'
     resourcetype_id = 'ibis'
-    
+
     registerSchema('xsd' + os.sep + 'ibis.xsd', 'XMLSchema')
-    registerStylesheet('xslt' + os.sep + 'ibis_metadata.xslt', 
+    registerStylesheet('xslt' + os.sep + 'ibis_metadata.xslt',
         'metadata')
-    registerStylesheet('xslt' + os.sep + 'ibis_displacement_metadata.xslt', 
+    registerStylesheet('xslt' + os.sep + 'ibis_displacement_metadata.xslt',
         'metadatadisplacement')
-    
+
     registerIndex('project_id', '/GBSAR_IBIS/@project_id', 'text')
     registerIndex('volcano_id', '/GBSAR_IBIS/@volcano_id', 'text')
-    registerIndex('start_datetime', '/GBSAR_IBIS/image_information/master_image/start_datetime/value', 'datetime')
-    registerIndex('end_datetime', '/GBSAR_IBIS/image_information/slave_image/start_datetime/value', 'datetime')
-    registerIndex('upperleft_latitude', 
-        '/GBSAR_IBIS/image_information/range_upperleft/latitude/value', 
+    registerIndex('start_datetime',
+        '/GBSAR_IBIS/image_information/master_image/start_datetime/value',
+        'datetime')
+    registerIndex('end_datetime',
+        '/GBSAR_IBIS/image_information/slave_image/start_datetime/value',
+        'datetime')
+    registerIndex('upperleft_latitude',
+        '/GBSAR_IBIS/image_information/range_upperleft/latitude/value',
         'float')
-    registerIndex('upperleft_longitude', 
-        '/GBSAR_IBIS/image_information/range_upperleft/longitude/value', 
+    registerIndex('upperleft_longitude',
+        '/GBSAR_IBIS/image_information/range_upperleft/longitude/value',
         'float')
-    registerIndex('lowerright_latitude', 
-        '/GBSAR_IBIS/image_information/range_lowerright/latitude/value', 
+    registerIndex('lowerright_latitude',
+        '/GBSAR_IBIS/image_information/range_lowerright/latitude/value',
         'float')
-    registerIndex('lowerright_longitude', 
-        '/GBSAR_IBIS/image_information/range_lowerright/longitude/value', 
+    registerIndex('lowerright_longitude',
+        '/GBSAR_IBIS/image_information/range_lowerright/longitude/value',
         'float')
-    registerIndex('local_path_image_quality', 
+    registerIndex('local_path_image_quality',
         '/GBSAR_IBIS/files/file/local_path[../@id="coherence"]', 'text')
-    registerIndex('local_path_image_displacement', 
+    registerIndex('local_path_image_displacement',
         '/GBSAR_IBIS/files/file/local_path[../@id="losdisplacement"]', 'text')
-    registerIndex('local_path_image_phase', 
+    registerIndex('local_path_image_phase',
         '/GBSAR_IBIS/files/file/local_path[../@id="phase"]', 'text')
 
 
@@ -70,67 +74,76 @@ class _IBISGeoTIFFMapperBase(object):
     A base mapper class where other IBIS mapper may inherit from.
     """
     implements(IMapper)
-    
+
     def process_GET(self, request):
-        # parse input arguments
-        pid = request.args0.get('project_id', '')
-	try:
-            start = UTCDateTime(request.args0.get('start_datetime')).isoformat()
-	    end = request.args0.get('end_datetime', False)
-	    if end:
-	        end = UTCDateTime(end).isoformat()
-	    else:
-	        end = UTCDateTime().isoformat()
-	except:
-	    start = False
-	    end = False
+        tab = Table('/exupery/ibis', request.env.db.metadata, autoload=True)
+        # fetch arguments
+        try:
+            limit = int(request.args0.get('limit'))
+            offset = int(request.args0.get('offset', 0))
+        except:
+            limit = None
+            offset = 0
+        oncl = sql.and_(1 == 1)
+        # build up query
+        columns = [tab.c['document_id'], tab.c['project_id'],
+                   tab.c['start_datetime'], tab.c['end_datetime'],
+                   tab.c[self.type_id]]
+        query = sql.select(columns, oncl, limit=limit, distinct=True,
+                           offset=offset, order_by=tab.c['start_datetime'])
+        # process arguments
+        try:
+            temp = request.args0.get('project_id', '')
+            query = query.where(tab.c['project_id'] == temp)
+        except:
+            pass
+        try:
+            temp = UTCDateTime(request.args0.get('start_datetime'))
+            query = query.where(tab.c['start_datetime'] >= temp.datetime)
+        except:
+            pass
+        try:
+            temp = UTCDateTime(request.args0.get('end_datetime'))
+            query = query.where(tab.c['start_datetime'] < temp.datetime)
+        except:
+            pass
         # generate XML result
         xml = Element("query")
-        # build up and execute query
-	SQL = """
-           SELECT 
-               document_id, 
-               start_datetime, 
-               end_datetime,
-               %s
-           FROM "/exupery/ibis"
-           WHERE project_id = :pid 
-           AND %s IS NOT NULL
-	"""
-	if start:
-	   SQL += """
-	   AND start_datetime >= :start
-	   AND end_datetime < :end
-	   """
-        query = sql.text(SQL % (self.type, self.type))
+        # execute query
         try:
-	    if start:
-                 result = self.env.db.query(query, pid=pid, start=start, end=end)
-	    else:
-	         result = self.env.db.query(query, pid=pid)
+            results = request.env.db.query(query)
         except:
             return toString(xml)
-        
-        for i in result:
+        for i in results:
             s = Sub(xml, "resource", document_id=str(i.document_id))
-            Sub(s, 'start_datetime').text = (i.start_datetime).isoformat()
-            Sub(s, 'end_datetime').text = (i.end_datetime).isoformat()
-            Sub(s, 'url').text = 'local://' + i[self.type]
+            try:
+                temp = i.start_datetime.isoformat()
+            except:
+                temp = ''
+            Sub(s, 'start_datetime').text = temp
+            try:
+                temp = i.end_datetime.isoformat()
+            except:
+                temp = ''
+            Sub(s, 'end_datetime').text = temp
+            Sub(s, 'url').text = 'local://' + i[self.type_id]
         return toString(xml)
+
+
 
 class IBISQualityGeoTIFFMapper(Component, _IBISGeoTIFFMapperBase):
     """
     Returns a list of filtered IBIS Quality GeoTiff files.
     """
-    type = 'local_path_image_quality'
+    type_id = 'local_path_image_quality'
     mapping_url = '/exupery/wp1/ibis/quality/geotiff'
-    
+
 
 class IBISLosDisplacementGeoTIFFMapper(Component, _IBISGeoTIFFMapperBase):
     """
     Returns a list of filtered IBIS LosDisplacement GeoTiff files.
     """
-    type = 'local_path_image_displacement'
+    type_id = 'local_path_image_displacement'
     mapping_url = '/exupery/wp1/ibis/losdisplacement/geotiff'
 
 
@@ -138,5 +151,5 @@ class IBISPhaseGeoTIFFMapper(Component, _IBISGeoTIFFMapperBase):
     """
     Returns a list of filtered IBIS Phase GeoTiff files.
     """
-    type = 'local_path_image_phase'
+    type_id = 'local_path_image_phase'
     mapping_url = '/exupery/wp1/ibis/phase/geotiff'
